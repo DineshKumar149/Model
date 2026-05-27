e2b4892bf3a17cf7b53c5576809694e4793b8bfa
novaai112 <dineshkumar2729305@gmail.com>
2026-05-27 17:19:38 +0530

    Fix React is not defined error for Filerobot component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+// Inject React globally to fix "React is not defined" in older third-party libraries (like Filerobot dependencies)
+(window as any).React = React;
 
 createRoot(document.getElementById("root")!).render(<App />);
